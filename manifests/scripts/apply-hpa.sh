6bea9f993e1a763a6e9b108585316284595ac533
Daniel Siebert <dnsbrt86@gmail.com>
2026-02-11 10:55:56 +0100

    cleanup

--- manifests/scripts/apply-hpa.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-kubectl apply -f manifests/variants/50-hpa.yaml
-kubectl get hpa -n hands-on
-
--- manifests/scripts/apply-v2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-kubectl apply -f manifests/base/00-namespace.yaml
-kubectl apply -f manifests/base/01-service.yaml
-kubectl apply -f manifests/variants/20-deploy-v2-resources-probes.yaml
-
-kubectl get pods -n hands-on -o wide
-
