8f74b9cae5e34ea5541a9ae7a4777eb51a4878d1
Daniel Siebert <dnsbrt86@gmail.com>
2026-02-11 08:42:35 +0100

    prepare hello

--- gke/00-cluster-create.sh
+++ gke/00-cluster-create.sh
@@ -11,7 +11,7 @@ gcloud container clusters create "${CLUSTER_NAME}" \
   --project "${PROJECT_ID}" \
   --zone "${ZONE}" \
   --release-channel "regular" \
-  --machine-type "e2-standard-2" \
+  --machine-type "e2-standard-4" \
   --num-nodes "1" \
   --enable-ip-alias \
   --workload-pool="${PROJECT_ID}.svc.id.goog" \
